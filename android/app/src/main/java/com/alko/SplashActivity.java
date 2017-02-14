90ebc5ca4efe5645f335929f4b20d0a414fa0839
SoftwareKings <dragonfighter0120@gmail.com>
2017-02-14 17:12:08 +0700

    Add splash screen

---
+++ android/app/src/main/java/com/alko/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.alko;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
