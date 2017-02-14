f1383bff7fa56fb091a5a3efd541bf90a5cbd339
SoftwareKings <dragonfighter0120@gmail.com>
2017-02-14 20:07:29 +0700

    Handle restricted for location permission

--- App/Lib/LocationHelper.js
+++ App/Lib/LocationHelper.js
@@ -1,9 +1,14 @@
 import Permissions from 'react-native-permissions';
 
-export const requestPermission = (type = 'whenInUse') => new Promise((success) => {
+export const requestPermission = (type = 'whenInUse') => new Promise((success, reject) => {
   Permissions.getPermissionStatus('location', type)
     .then((response) => {
-      if (response !== 'authorized') {
+      if (response === 'restricted') {
+        // restricted:
+        // (iOS only) user is not able to grant this permission, either because it's
+        // not supported by the device or because it has been blocked by parental controls.
+        reject(response);
+      } else if (response !== 'authorized') {
         Permissions.requestPermission('location', type)
         .then((permissionResponse) => {
           if (permissionResponse !== 'authorized') {
