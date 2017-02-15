87786429e68266135b2d105b931405b410d41713
SoftwareKings <dragonfighter0120@gmail.com>
2017-02-15 23:43:25 +0700

    Change transition animation style

--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -30,14 +30,71 @@ import IntroductionStep4Screen from '../Containers/Introduction/Step4';
 * Documentation: https://github.com/aksonov/react-native-router-flux
 ***************************/
 
+const customAnimationStyle = (props) => {
+  const { layout, position, scene } = props;
+
+  const direction = (scene.navigationState && scene.navigationState.direction) ?
+    scene.navigationState.direction : 'horizontal';
+
+  const index = scene.index;
+  const inputRange = [index - 1, index, index + 1];
+  const width = layout.initWidth;
+  const height = layout.initHeight;
+
+  const opacity = position.interpolate({
+    inputRange,
+    //  default: outputRange: [1, 1, 0.3],
+    outputRange: [1, 1, 0.95],
+  });
+
+  const scale = position.interpolate({
+    inputRange,
+    //  default: outputRange: [1, 1, 0.95],
+    outputRange: [1, 1, 1],
+  });
+
+  let translateX = 0;
+  let translateY = 0;
+
+  switch (direction) {
+    case 'horizontal':
+      translateX = position.interpolate({
+        inputRange,
+        //  default: outputRange: [width, 0, -10],
+        outputRange: [width, 0, -width],
+      });
+      break;
+    case 'vertical':
+      translateY = position.interpolate({
+        inputRange,
+        //  default: outputRange: [height, 0, -10],
+        outputRange: [height, 0, 0],
+      });
+      break;
+    default:
+      break;
+  }
+
+  return {
+    opacity,
+    transform: [
+      { scale },
+      { translateX },
+      { translateY },
+    ],
+  };
+};
+
 class NavigationRouter extends Component {
   render() {
     return (
-      <Router>
-        <Scene initial key="introStep1Screen" component={IntroductionStep1Screen} hideNavBar />
-        <Scene key="introStep2Screen" component={IntroductionStep2Screen} hideNavBar />
-        <Scene key="introStep3Screen" component={IntroductionStep3Screen} hideNavBar />
-        <Scene key="introStep4Screen" component={IntroductionStep4Screen} hideNavBar />
+      <Router animationStyle={customAnimationStyle}>
+        <Scene key="onboard">
+          <Scene initial key="introStep1Screen" component={IntroductionStep1Screen} hideNavBar />
+          <Scene key="introStep2Screen" component={IntroductionStep2Screen} hideNavBar />
+          <Scene key="introStep3Screen" component={IntroductionStep3Screen} hideNavBar />
+          <Scene key="introStep4Screen" component={IntroductionStep4Screen} hideNavBar />
+        </Scene>
 
         <Scene key="drawer" component={NavigationDrawer} open={false}>
           <Scene key="drawerChildrenWrapper" navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
