98193215e58f0cce03fb98b1dc848df1071fcc98
SoftwareKings <dragonfighter0120@gmail.com>
2017-02-15 23:43:46 +0700

    Change transition animation style

--- App/Themes/ApplicationStyles.js
+++ App/Themes/ApplicationStyles.js
@@ -23,7 +23,7 @@ const ApplicationStyles = {
     },
     container: {
       flex: 1,
-      paddingTop: Metrics.baseMargin
+      paddingTop: Metrics.baseMargin,
     },
     section: {
       margin: Metrics.section,
