9900bd26f42c9f7e07c5ba2771b2b32342e755e6
SoftwareKings <dragonfighter0120@gmail.com>
2017-03-11 09:21:14 -0400

    Alko Special Waning

---
+++ App/Components/Dialogs/AlkoSpecialWarningDialog.js
@@ -0,0 +1,33 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  Text,
+} from 'react-native';
+import I18n from 'react-native-i18n';
+
+import Button from '../Button';
+import Dialog from '../Dialog';
+import styles from '../Styles/AlkoSpecialWarningDialogStyle';
+
+export default class AlkoSpecialWarningDialog extends Component {
+
+  static propTypes = {
+    onClose: PropTypes.func,
+    onButtonPress: PropTypes.func,
+    visible: PropTypes.bool,
+  }
+
+  render() {
+    return (
+      <Dialog
+        closeButton
+        closeOnBackdropPress
+        onClose={this.props.onClose}
+        visible={this.props.visible}
+      >
+        <Text style={styles.title}>{I18n.t('Redeem_WarningTitle')}</Text>
+        <Text style={styles.body}>{I18n.t('Redeem_WarningBody')}</Text>
+        <Button style={styles.button} onPress={this.props.onButtonPress} text={I18n.t('Redeem_WarningDismiss')} />
+      </Dialog>
+    );
+  }
+}
